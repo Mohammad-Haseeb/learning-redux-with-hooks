6a1f989cfee29b3e635832af6e2eed6c8566a3f4
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-02-05 10:40:59 +0500

    Components

---
+++ src/Components/Counter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Counter(){
+  return(
+      <>
+          <button>Increment</button> <br/>
+          <button>Decrement</button>
+      </>
+  );
+}
---
+++ src/Components/Display.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Display(){
+  return(
+      <>
+       <h1>Value : 3</h1>
+      </>
+  );
+}
